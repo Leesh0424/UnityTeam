b950edd2851ada979feaff5d51cfd4ea61cec10b
DESKTOP-F5DK0IO\User <a46639273@gmail.com>
2022-11-29 15:03:16 +0900

    no message

--- Assets/02. Scripts/PlayerCtr.cs
+++ Assets/02. Scripts/PlayerCtr.cs
@@ -5,16 +5,76 @@ using UnityEngine.EventSystems;
 
 public class PlayerCtr : MonoBehaviour
 {
-    private int hp = 1;
+    public GameObject Cam; 
+    public CharacterController SelectPlayer; 
+    public float Speed; 
+    public float JumpPow;
 
-    public float speed = 5f;    //속도
+    private float Gravity; 
+    private Vector3 MoveDir;
+    private bool JumpButtonPressed;  
+    private bool FlyingMode;  
 
-    void FixedUpdate()
+    void Start()
     {
-        float hor = Input.GetAxis("Horizontal");        //왼쪽, 오른쪽 키 
-        float ver = Input.GetAxis("Vertical");          //앞, 뒤 키
+        Speed = 5.0f;
+        Gravity = 10.0f;
+        MoveDir = Vector3.zero;
+        JumpPow = 5.0f;
+        JumpButtonPressed = false;
+        FlyingMode = false;
+    }
 
-        transform.Translate(Vector3.forward * speed * ver * Time.deltaTime);      //이동
-        transform.Rotate(Vector3.up * speed * hor);    // 회전
+    void Update()
+    {
+        if (SelectPlayer == null) return;
+
+        if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+        {
+            var offset = Cam.transform.forward;
+            offset.y = 0;
+            transform.LookAt(SelectPlayer.transform.position + offset);
+        }
+        if (SelectPlayer.isGrounded)
+        {
+            MoveDir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            MoveDir = SelectPlayer.transform.TransformDirection(MoveDir);
+            MoveDir *= Speed;
+
+            if (JumpButtonPressed == false && Input.GetButton("Jump"))
+            {
+                SelectPlayer.transform.rotation = Quaternion.Euler(0, 45, 0);
+                JumpButtonPressed = true;
+                MoveDir.y = JumpPow;
+            }
+        }
+        else
+        {
+            if (MoveDir.y < 0 && JumpButtonPressed == false && Input.GetButton("Jump"))
+            {
+                FlyingMode = true;
+            }
+
+            if (FlyingMode)
+            {
+                JumpButtonPressed = true;
+
+                MoveDir.y *= 0.95f;
+
+                if (MoveDir.y > -1) MoveDir.y = -1;
+
+                MoveDir.x = Input.GetAxis("Horizontal");
+                MoveDir.z = Input.GetAxis("Vertical");
+            }
+            else
+                MoveDir.y -= Gravity * Time.deltaTime;
+        }
+
+        if (!Input.GetButton("Jump"))
+        {
+            JumpButtonPressed = false;  
+            FlyingMode = false;        
+        }
+        SelectPlayer.Move(MoveDir * Time.deltaTime);
     }
 }
